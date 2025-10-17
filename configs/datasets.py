a0a5e8a9ff7dd3bb03da5c25bfb10ea5b7802e38
davidwarrenstudent <david.warren@students.mq.edu.au>
2025-10-17 13:55:57 +1100

    initial commit

---
+++ configs/datasets.py
@@ -0,0 +1,50 @@
+# Datsets config
+wmt14 = {"clean_name": "wmt14fr-en",
+        "dataset_name": "fr-en",
+        "dataset_location": "wmt/wmt14",
+        "options": None,
+        "subset": "test",
+        "task_type": "translation",
+        "dict_ans": False}
+
+triviaqa = {"clean_name": "TriviaQA",
+            "dataset_name": "rc",
+            "dataset_location": "mandarjoshi/trivia_qa",
+            "options": None,
+            "subset": "train",
+            "task_type": "qa",
+          "dict_ans": True}
+
+truthfulqa = {"clean_name": "TruthfulQA",
+             "dataset_name": "generation",
+             "dataset_location": "truthfulqa/truthful_qa",
+             "options": None,
+             "subset": "validation",
+              "task_type": "qa",
+          "dict_ans": True}
+
+wmt14ru = {"clean_name": "wmt14ru-en",
+        "dataset_name": "ru-en",
+        "dataset_location": "wmt/wmt14",
+        "options": None,
+        "subset": "test",
+        "task_type": "translation",
+        "dict_ans": False}
+
+
+wmt19de = {"clean_name": "wmt19de-en",
+        "dataset_name": "de-en",
+        "dataset_location": "wmt/wmt19",
+        "options": None,
+        "subset": "train",
+        "task_type": "translation",
+        "dict_ans": False}
+
+
+sciq = {"clean_name": "SciQ",
+        "dataset_name": "default",
+        "dataset_location": "allenai/sciq",
+        "options": None,
+        "subset": "train",
+        "task_type": "qa",
+        "dict_ans": False}
---
+++ configs/models.py
@@ -0,0 +1,49 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoModelForSequenceClassification, AutoConfig, BitsAndBytesConfig, GenerationConfig 
+
+double_quant_cfg = BitsAndBytesConfig(
+   load_in_4bit=True,
+   bnb_4bit_quant_type="nf4",
+   bnb_4bit_use_double_quant=True,
+   bnb_4bit_compute_dtype=torch.bfloat16,
+   llm_int8_enable_fp32_cpu_offload=True
+)
+
+single_quant_cfg = BitsAndBytesConfig(
+   load_in_8bit=True,
+   llm_int8_enable_fp32_cpu_offload=True
+)
+
+llama7b_chat = {"model_name": "meta-llama/Llama-2-7b-chat-hf",
+                 "hf_model_func": AutoModelForCausalLM,
+                 "bnb_config": double_quant_cfg,
+                 "block_limit": None
+                 }
+
+llama13b_chat = {"model_name": "meta-llama/Llama-2-13b-chat-hf",
+                 "hf_model_func": AutoModelForCausalLM,
+                 "bnb_config": double_quant_cfg,
+                 "block_limit": None
+                 }
+
+
+llama13b_chat = {"model_name": "meta-llama/Llama-2-13b-chat-hf",
+                 "hf_model_func": AutoModelForCausalLM,
+                 "bnb_config": double_quant_cfg,
+                 "block_limit": None
+                 }
+llama70b_chat = {"model_name": "meta-llama/Llama-2-70b-chat-hf",
+                 "hf_model_func": AutoModelForCausalLM,
+                 "bnb_config": double_quant_cfg,
+                 "block_limit": None
+                 }
+
+t5_base = {"model_name": "google/flan-t5-base",
+           "hf_model_func": AutoModelForSeq2SeqLM,
+           "bnb_config": None,
+           "block_limit": None}
+
+t5_xl = {"model_name": "google/flan-t5-xl",
+           "hf_model_func": AutoModelForSeq2SeqLM,
+           "bnb_config": None,
+           "block_limit": None}
