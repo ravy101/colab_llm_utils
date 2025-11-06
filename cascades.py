4d6adb3193370560d14c2be741b01956d3efbf62
davidwarrenstudent <david.warren@students.mq.edu.au>
2025-11-06 18:25:47 +1100

    Update cascades.py

--- cascades.py
+++ cascades.py
@@ -1,43 +1,43 @@
 import numpy as np
 from . import misc
 
-def cascade_scored_samples(df, col, metric, ml_suffix='_13b', ):
-    print(f"delegating by {col}")
+def cascade_scored_samples(df, col, metric, ml_suffix='_13b'):
+    print(f"deferring by {col}")
     thresh = np.linspace(0- .001, 1 +0.001,100)
     accs = []
-    n_delegated = []
-    p_delegated = []
+    n_deferred = []
+    p_deferred = []
     accept_acc = []
-    delegated_acc = []
-    delegated_correct = []
+    deferred_acc = []
+    deferred_correct = []
     coverage = []
     gains = []
     small_correct = df[metric].mean()
     for t in thresh:
-        delegate_idx = df[col] > (1-t)
-        #delegate_idx = df_full[DELEGATE_COLUMN] <= t
-        correct_7 = df[metric][~delegate_idx].sum()
-        correct_70 = df[metric + ml_suffix][delegate_idx].sum()
-        accept_acc.append(df[metric][~delegate_idx].mean())
-        delegated_acc.append(df[metric + ml_suffix][delegate_idx].mean())
-        delegated_correct.append(df[metric + ml_suffix][delegate_idx].sum())
-        n_delegated.append(delegate_idx.sum())
-        p_delegated.append((delegate_idx.sum()/len(df)))
-        coverage.append((~delegate_idx).mean())
+        defer_idx = df[col] > (1-t)
+        #defer_idx = df_full[defer_COLUMN] <= t
+        correct_7 = df[metric][~defer_idx].sum()
+        correct_70 = df[metric + ml_suffix][defer_idx].sum()
+        accept_acc.append(df[metric][~defer_idx].mean())
+        deferred_acc.append(df[metric + ml_suffix][defer_idx].mean())
+        deferred_correct.append(df[metric + ml_suffix][defer_idx].sum())
+        n_deferred.append(defer_idx.sum())
+        p_deferred.append((defer_idx.sum()/len(df)))
+        coverage.append((~defer_idx).mean())
         accs.append((correct_7 + correct_70)/len(df))
         gains.append(accs[-1] - small_correct)
-        if len(p_delegated) > 1:
-            if p_delegated[-2] < .2 and p_delegated[-1] >= .2:
-                p_del_20 = p_delegated.copy()
+        if len(p_deferred) > 1:
+            if p_deferred[-2] < .2 and p_deferred[-1] >= .2:
+                p_del_20 = p_deferred.copy()
                 accs_20 = accs.copy()
                 p_del_20, accs_20 = misc.cap_interp_curve(p_del_20, accs_20, .2)
                 auc_20 = np.trapezoid(accs_20, x= p_del_20)
-            if p_delegated[-2] < .4 and p_delegated[-1] >= .4:
-                p_del_40 = p_delegated.copy()
+            if p_deferred[-2] < .4 and p_deferred[-1] >= .4:
+                p_del_40 = p_deferred.copy()
                 accs_40 = accs.copy()
                 p_del_40, accs_40 = misc.cap_interp_curve(p_del_40, accs_40, .4)
                 auc_40 = np.trapezoid(accs_40, x= p_del_40)
 
-    return {"p_delegated": p_delegated, "n_delegated":n_delegated, "delegated_correct": delegated_correct, "delegated_acc":delegated_acc, "accs": accs, "gains":gains, "auc": np.trapezoid(accs, x= p_delegated),
+    return {"p_deferred": p_deferred, "n_deferred":n_deferred, "deferred_correct": deferred_correct, "deferred_acc":deferred_acc, "accs": accs, "gains":gains, "auc": np.trapezoid(accs, x= p_deferred),
             "auc_20": auc_20, "auc_40": auc_40, "accs_20": accs_20, "accs_40": accs_40, "p_del_20": p_del_20, "p_del_40": p_del_40}
 
