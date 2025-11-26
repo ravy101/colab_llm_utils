557f8239a79f1b1afeb7ecd8498b1e307f37b5f0
MR David Warren <45939225@students.mq.edu.au>
2025-11-26 12:26:32 +1100

    Update confidence.py

--- confidence.py
+++ confidence.py
@@ -30,7 +30,7 @@ def get_embedding_dict_glove(df, glove, tokenizer, suffix = '', glove_dim = 100)
                     try:
                         emb_dict[k] = glove[tokenizer.decode(k).lower()]
                     except:
-                        emb_dict[k] = np.zeroes(glove_dim)
+                        emb_dict[k] = np.zeros(glove_dim)
     for token_outs in df['token_outs' + suffix]:
         for token in token_outs:
             if token.item() not in emb_dict:
