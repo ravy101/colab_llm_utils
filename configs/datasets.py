8154547a82f885d77cd920d8c8e4d5cb5747838d
MR David Warren <45939225@students.mq.edu.au>
2026-02-25 15:31:40 +1100

    Update datasets.py

--- configs/datasets.py
+++ configs/datasets.py
@@ -140,7 +140,7 @@ def doc_to_text_nq(item):
   text = f"""
   Please provide the specific answer to the following question. Do not include reasoning, explanation or conversational filler. Output only the required information as concisely as possible. If unsure or unable to answer, output only your best guess.
   Question: ({item['question']}?)
-Short Answer: ("""
+Short Answer:"""
   return text
 
 
