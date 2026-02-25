62234539e5b769019a7725cad72b460d0092caf9
MR David Warren <45939225@students.mq.edu.au>
2026-02-25 14:45:02 +1100

    Update datasets.py

--- configs/datasets.py
+++ configs/datasets.py
@@ -138,7 +138,7 @@ def doc_to_text_qa(item):
 
 def doc_to_text_nq(item):
   text = f"""
-  Please provide the specific answer to the following question. Do not include reasoning, explanation or conversational filler. Output only the required information as concisely as possible.
+  Please provide the specific answer to the following question. Do not include reasoning, explanation or conversational filler. Output only the required information as concisely as possible. If unsure or unable to answer, output only your best guess.
   Question: ({item['question']}?)
 Short Answer: ("""
   return text
