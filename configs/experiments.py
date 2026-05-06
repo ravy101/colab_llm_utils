dab41b35de7137a1d579a2d5d5c6e61e27c075b8
MR David Warren <45939225@students.mq.edu.au>
2026-05-06 17:21:22 +1000

    Update experiments.py

--- configs/experiments.py
+++ configs/experiments.py
@@ -2,10 +2,9 @@ from . import datasets, models, inference
 
 
 base_qwen_2ax_trivia = {
-    dataset_config = datasets.triviaqa,
-    origin_config ={ "model": models.qwen3_8b,
-    "thinking": False,
-    "rag": False
+        "dataset_config": datasets.triviaqa,
+        "origin_config": { "model": models.qwen3_8b,
+        "thinking": False,
+        "rag": False
+        }
     }
-
-}
