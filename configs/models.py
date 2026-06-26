5551ae2eeb3dd40672359fdafb94ea89a7dfc68f
davidwarrenstudent <david.warren@students.mq.edu.au>
2026-06-26 10:57:55 +1000

    Update models.py

--- configs/models.py
+++ configs/models.py
@@ -209,6 +209,6 @@ qwen3_14b = {
 qwen3_32b = {
     "model_name": "Qwen/Qwen3-32B",
     "hf_model_func": AutoModelForCausalLM,
-    "bnb_config": double_quant_cfg,
+    "bnb_config": single_quant_cfg,
     "block_limit": None
 }
